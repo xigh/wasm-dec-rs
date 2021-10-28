9813487ef0651107ee484610ddc925adea9763db
Philippe Anel <zexigh@gmail.com>
2021-10-28 20:10:43 +0200

    open and read file

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
+// use std::vec;
 
 fn main() {
     for (index, argument) in env::args().enumerate() {
         if index > 0 {
-            let rval = process(&argument);
-            match rval {
-                Err(err) => {
-                    eprintln!("process \"{}\" failed with: {}", argument, err)
-                }
-                _ => {}
+            if let Err(err) = process(&argument) {
+                eprintln!("process \"{}\" failed with: {}", argument, err)
             }
         }
     }
 }
 
-fn process(_name: &String) -> std::result::Result<bool, &'static str> {
-    return Err("not implemented")
+fn process(name: &String) -> std::result::Result<(), String> {
+    let f = File::open(name);
+    if let Err(err) = f {
+        return Err(err.to_string());
+    }
+
+    let mut reader = BufReader::new(f.unwrap());
+    let mut buffer = Vec::new();
+
+    if let Err(err) = reader.read_to_end(&mut buffer) {
+        return Err(err.to_string());
+    }
+
+    for value in buffer {
+        println!("{}", value);
+    }
+
+    Ok(())
 }
