65a4624f2195a0193685da1b5c9202a6562e7f8b
Philippe Anel <zexigh@gmail.com>
2021-10-28 19:09:57 +0200

    added process function, with Result returned value

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,17 @@ use std::env;
 fn main() {
     for (index, argument) in env::args().enumerate() {
         if index > 0 {
-            println!("{}", argument);
+            let rval = process(&argument);
+            match rval {
+                Err(err) => {
+                    eprintln!("process \"{}\" failed with: {}", argument, err)
+                }
+                _ => {}
+            }
         }
     }
+}
+
+fn process(_name: &String) -> std::result::Result<bool, &'static str> {
+    return Err("not implemented")
 }
